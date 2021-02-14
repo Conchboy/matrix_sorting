1194608b6bffcc8f02e1c51083ceaa7065f7d8f8
conchboy <panchaocn@hotmail.com>
2021-02-14 09:01:56 +0800

    Thanks to the plug in C/C++ single file execution, just use the hotkey Alt+E and use run! no need to open multi-projects now!

---
+++ Others/test.cpp
@@ -0,0 +1,12 @@
+# include <stdio>
+# include <iostream>
+# include <cstdlib>
+
+using namespace std;
+
+int main(void){
+    cout << "hello this is Richard" << endl;
+
+    return 0;
+
+}
---
+++ parrell.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 实现将数组中的n个数据从大到小排序, 排序后的数组元素再主函数中输入
+// 将结果显示再屏幕上和文件P9_1.out中
+void sort(int *x, int n)
+{
+    int i, j, k, t;
+    for (i = 0; i < n -1; i++)
+    {
+        k = 1;
+        for (j = i +1; j < n; j++)
+            if (x[j] > x[k]) k = j;
+        if (k != i)
+        {
+            t = x[i];
+            x[i] = x[k];
+            x[k] = t;
+        }
+    }
+}
+
+int main() {
+    FILE *fp;
+    int *p, i, a[10];
+    fp = fopen("./p9_1.out", "w");
+    p = a;
+    printf("Input 10 numbers:");
+    for (i = 0; i < 10; i++)
+        scanf("%d", p++);
+    p = a;
+    sort(p,10);
+    for (; p < a + 10; p++)
+    {
+        printf("%d ", *p);
+        fprintf(fp, "%d ", *p);
+    }
+    system("pause");
+    fclose(fp);
+}
